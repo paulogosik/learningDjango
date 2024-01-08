d86e11249e886387260ac861e4b8e55dc1344a27
Paulo Gosik M. Moita <paulogosikmoita@gmail.com>
2024-01-08 10:31:37 -0300

    update files

--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html', {'name': 'Negueba'})
+    # return render(request, 'index.html', {'name': 'Negueba'}) -> Você pode passar uma variável que será usada no 'index.html'
+    return render(request, 'index.html')
